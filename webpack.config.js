be28f35ed9032f31fcdd62b160bfb0f8fba66c0e
arthurgomespassos <arthurgomespassos74@gmail.com>
2022-02-25 19:17:48 -0300

    corrigido erro de css não carregar na home

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,11 @@ module.exports = {
           presets: ['@babel/env']
         }
       }
-    }/*, 
+    },
     {
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
-    }*/]
+    }]
   },
   devtool: 'source-map'
 };
